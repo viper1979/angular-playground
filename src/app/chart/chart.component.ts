5e2a46fcf58dd2f85bb6aac7ebfb84dbd1496e37
Viper <viper_bite@gmx.de>
2017-11-02 18:06:56 +0100

    rearrange components + implemented timeframes for chart

--- src/app/chart/chart.component.ts
+++ src/app/chart/chart.component.ts
@@ -19,13 +19,15 @@ export class ChartComponent implements OnInit, OnChanges, OnDestroy {
   @ViewChild('volumechart')
   volumeChart: UIChart;
 
-  chartData: PrimeNgChartData;
-  chartDataVolume: PrimeNgChartData;
-
-  volumeChartOptions: any;
-
   @Input()
   symbol: string;
+
+  chartData: PrimeNgChartData;
+  chartDataVolume: PrimeNgChartData;
+  volumeChartOptions: any;
+
+  selectedTimeframe: string = '1m';
+  availableTimeframes: any[];
 
   constructor(private _bitfinexService: BitfinexService) {
   }
@@ -44,66 +46,26 @@ export class ChartComponent implements OnInit, OnChanges, OnDestroy {
         display: false
       }
     };
+
+    this.availableTimeframes = [
+      {label: 'one minute', value: '1m'},
+      {label: 'five minutes', value: '5m'},
+      {label: '15 minutes', value: '15m'},
+      {label: '30 minutes', value: '30m'},
+      {label: 'one hour', value: '1h'},
+      {label: '3 hours', value: '3h'},
+      {label: '6 hours', value: '6h'},
+      {label: '12 hours', value: '12h'},
+      {label: 'one day', value: '1D'},
+      {label: 'one week', value: '7D'},
+      {label: 'two weeks', value: '14D'},
+      {label: 'one month', value: '1M'}
+    ];
   }
 
   ngOnChanges(changes: SimpleChanges) {
     if (changes.symbol.currentValue.length === 6) {
-      if (this._bitfinexSubscription) {
-        this._bitfinexService.unsubscribe( this._bitfinexSubscription );
-      }
-
-      if (!this.chartData) {
-        this.chartData = new PrimeNgChartData( );
-        this.chartDataVolume = new PrimeNgChartData( );
-      }
-      this.chartData.datasets[0].label = this.symbol;
-      this.chartDataVolume.datasets[0].label = 'volume';
-
-      console.log( 'ChartComponent | ngOnChanges | Trying to subscribe to symbol: ' + this.symbol);
-      this._bitfinexSubscription = this._bitfinexService.getCandleListener( this.symbol );
-
-      this._bitfinexSubscription.heartbeat.subscribe(
-        hb => console.log( 'ChartComponent | Channel \'' + hb.channel + '\' heartbeat @ ' + hb.timestamp )
-      );
-
-      this._bitfinexSubscription.listener.subscribe(
-        next => {
-          let candleMessage: CandleMessage = next as CandleMessage;
-
-          this._chartData.set(candleMessage.timestamp.toString( ), candleMessage);
-          if (this._chartData.size > 20) {
-            let firstKey = Array.from( this._chartData.keys( ) ).sort( (d1, d2) => this.DateComparer( d1, d2) )[0];
-            this._chartData.delete(firstKey);
-          }
-
-          this.chartData.labels = [];
-          this.chartData.datasets[0].data = [];
-          this.chartData.datasets[0].label = this.symbol;
-
-          this.chartDataVolume.labels = [];
-          this.chartDataVolume.datasets[0].data = [];
-          this.chartDataVolume.datasets[0].label = this.symbol;
-
-          Array.from( this._chartData.keys( ) ).sort( (d1, d2) => this.DateComparer( d1, d2) ).forEach(element => {
-            let candle = this._chartData.get(element);
-
-            let displayTimestamp = ( candle.timestamp.getHours() < 10 ? '0' + candle.timestamp.getHours( ) : candle.timestamp.getHours( ) ) + ':';
-            displayTimestamp += candle.timestamp.getMinutes() < 10 ? '0' + candle.timestamp.getMinutes( ) : candle.timestamp.getMinutes( );
-
-            this.chartData.labels.push( displayTimestamp );
-            this.chartData.datasets[0].data.push( this._chartData.get(element).close );
-
-            this.chartDataVolume.labels.push( displayTimestamp );
-            this.chartDataVolume.datasets[0].data.push( this._chartData.get(element).volume );
-          });
-          this.chart.refresh( );
-          this.volumeChart.refresh( );
-
-          console.log( 'ChartComponent | ngOnChanges | message: ' + JSON.stringify(candleMessage) );
-        },
-        error => console.log( 'ChartComponent | ngOnChanges | error: ' + JSON.stringify(error) ),
-        () => console.log( 'ChartComponent | ngOnChanges | completed' )
-      );
+      this.initChart( );
     }
   }
 
@@ -111,6 +73,69 @@ export class ChartComponent implements OnInit, OnChanges, OnDestroy {
     if (this._bitfinexSubscription) {
       this._bitfinexService.unsubscribe(this._bitfinexSubscription);
     }
+  }
+
+  private initChart( ) {
+    if (this._bitfinexSubscription) {
+      this._bitfinexService.unsubscribe( this._bitfinexSubscription );
+    }
+
+    // if (!this.chartData) {
+    //   this.chartData = new PrimeNgChartData( );
+    //   this.chartDataVolume = new PrimeNgChartData( );
+    // }
+
+    this._chartData = new Map<string, CandleMessage>( );
+    this.chartData = new PrimeNgChartData( );
+    this.chartDataVolume = new PrimeNgChartData( );
+    this.chartData.datasets[0].label = this.symbol;
+    this.chartDataVolume.datasets[0].label = 'volume';
+
+    console.log( 'ChartComponent | ngOnChanges | Trying to subscribe to symbol: ' + this.symbol);
+    this._bitfinexSubscription = this._bitfinexService.getCandleListener( this.symbol, {timeframe: this.selectedTimeframe} );
+
+    this._bitfinexSubscription.heartbeat.subscribe(
+      hb => console.log( 'ChartComponent | Channel \'' + hb.channel + '\' heartbeat @ ' + hb.timestamp )
+    );
+
+    this._bitfinexSubscription.listener.subscribe(
+      next => {
+        let candleMessage: CandleMessage = next as CandleMessage;
+
+        this._chartData.set(candleMessage.timestamp.toString( ), candleMessage);
+        if (this._chartData.size > 200) {
+          let firstKey = Array.from( this._chartData.keys( ) ).sort( (d1, d2) => this.DateComparer( d1, d2) )[0];
+          this._chartData.delete(firstKey);
+        }
+
+        this.chartData.labels = [];
+        this.chartData.datasets[0].data = [];
+        this.chartData.datasets[0].label = this.symbol;
+
+        this.chartDataVolume.labels = [];
+        this.chartDataVolume.datasets[0].data = [];
+        this.chartDataVolume.datasets[0].label = this.symbol;
+
+        Array.from( this._chartData.keys( ) ).sort( (d1, d2) => this.DateComparer( d1, d2) ).forEach(element => {
+          let candle = this._chartData.get(element);
+
+          let displayTimestamp = ( candle.timestamp.getHours() < 10 ? '0' + candle.timestamp.getHours( ) : candle.timestamp.getHours( ) ) + ':';
+          displayTimestamp += candle.timestamp.getMinutes() < 10 ? '0' + candle.timestamp.getMinutes( ) : candle.timestamp.getMinutes( );
+
+          this.chartData.labels.push( displayTimestamp );
+          this.chartData.datasets[0].data.push( this._chartData.get(element).close );
+
+          this.chartDataVolume.labels.push( displayTimestamp );
+          this.chartDataVolume.datasets[0].data.push( this._chartData.get(element).volume );
+        });
+        this.chart.refresh( );
+        this.volumeChart.refresh( );
+
+        // console.log( 'ChartComponent | ngOnChanges | message: ' + JSON.stringify(candleMessage) );
+      },
+      error => console.log( 'ChartComponent | ngOnChanges | error: ' + JSON.stringify(error) ),
+      () => console.log( 'ChartComponent | ngOnChanges | completed' )
+    );
   }
 
   private DateComparer( date1: string, date2: string ): number {
@@ -124,6 +149,11 @@ export class ChartComponent implements OnInit, OnChanges, OnDestroy {
       return -1;
     }
     return 0;
+  }
+
+  timeframeChanged(event): void {
+    this.selectedTimeframe = event;
+    this.initChart( );
   }
 }
 
